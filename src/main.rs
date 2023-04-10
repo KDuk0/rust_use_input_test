cfc5c140a57e054479974715f4eba5fe64c33eb8
KDuk0 <125793699+KDuk0@users.noreply.github.com>
2023-04-10 11:13:53 +0200

    Writing user input into .txt file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
-use crate::utils::ask_for_a_number;
+use crate::utils::{ask_for_a_number, ask_for_a_string};
+use std::fs::write;
 
 mod utils;
 
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u8,
+}
+
 fn main() {
+
+    let first_name = ask_for_a_string("What is your first name?");
+    let last_name = ask_for_a_string("What is your last name?");
+    let age = ask_for_a_number("How old are you?").unwrap_or(0);
+    let person = Person {
+        first_name,
+        last_name,
+        age,
+    };
    
     match ask_for_a_number("How old are you?") {
      Ok(age) => println!("You are {} old", age),
      Err(err) => println!("{}", err),
     }
+
+    match write_person(&person) {
+        Ok(_) => println!("people.txt was written successfully"),
+        Err(err) => println!("There was error while writing people.txt: {}", err),
+    }
+}
+
+//adding new function that writes user input to .txt file; use push_str to add user input into string
+//std::io:Result<()> is the output we are returning
+fn write_person(person: &Person) -> std::io::Result<()> {
+    let mut output = String::new();
+    output.push_str(&person.first_name);
+    output.push('\n');
+    output.push_str(&person.last_name);
+    output.push('\n');
+    output.push_str(&person.age.to_string());
+    output.push('\n');
+    write("people.txt", output)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 pub fn read_string() -> String {
     let mut input = String::new();
     std::io::stdin()
@@ -13,7 +15,9 @@ pub fn read_number() -> Result<u8, String> {
         Err("You didn't enter any data".to_string())
     }
     else {
-        read_string().parse().or(Err("You've entered an invalid number. Please enter a value between 0 and 255".to_string()))
+        u8::from_str(&input).or(Err("You've entered an invalid number. Please enter a value between 0 and 255".to_string()))
+        // I used this but it messes up the input:
+        //read_string().parse().or(Err("You've entered an invalid number. Please enter a value between 0 and 255".to_string()))
     }
          
 
@@ -26,4 +30,9 @@ fn ask(question: &str) {
 pub fn ask_for_a_number(question: &str) -> Result<u8, String> {
    ask(question);
    read_number()
+}
+
+pub fn ask_for_a_string(question: &str) -> String {
+    ask(question);
+    read_string()
 }
