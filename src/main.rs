3428a56bca7acab1ad153c835481b47252f5e6ef
KDuk0 <125793699+KDuk0@users.noreply.github.com>
2023-04-20 13:33:43 +0200

    Finished

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,20 @@
-use crate::utils::{ask_for_a_number, ask_for_a_string};
-use std::fs::{write, read_to_string};
-use std::str::FromStr;
+use crate::utils::{read_people, write_people, Person};
 
 mod utils;
 
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u8,
-}
-
-impl Person {
-    fn print(&self) {
-        println!("{} {} {} old", &self.first_name, &self.last_name, &self.age);
-    }
-}
-
 fn main() {
 
-    let first_name = ask_for_a_string("What is your first name?");
-    let last_name = ask_for_a_string("What is your last name?");
-    let age = ask_for_a_number("How old are you?").unwrap_or(0);
-    let person = Person {
-        first_name,
-        last_name,
-        age,
-    };
-  
-    match write_person(&person) {
+    let person = Person::new();
+    let people = vec![person];
+      
+    match write_people(people) {
         Ok(_) => println!("people.txt was written successfully"),
         Err(err) => println!("There was error while writing people.txt: {}", err),
     }
 
-    match read_person() {
-        Ok(person) => {
-            println!("people.txt was read successfully:");
-            person.print();
-        },
+    match read_people() {
+        Ok(_people) => println!("people.txt was read successfully:"),
         Err(err) => println!("There was error while reading people.txt: {}", err),
     }
 }
 
-//adding new function that writes user input to .txt file; use push_str to add user input into string
-//std::io:Result<()> is the output we are returning
-fn write_person(person: &Person) -> std::io::Result<()> {
-    let mut output = String::new();
-    output.push_str(&person.first_name);
-    output.push('\n');
-    output.push_str(&person.last_name);
-    output.push('\n');
-    output.push_str(&person.age.to_string());
-    output.push('\n');
-    write("people.txt", output)
-}
-
-fn read_person() -> Result<Person, std::io::Error> {
-    let input = read_to_string("people.txt")?;
-    let mut lines = input.split('\n');
-    let first_name = lines.next().unwrap_or("").to_string();
-    let last_name = lines.next().unwrap_or("").to_string();
-    let age_as_string = lines.next().unwrap_or("0").to_string();
-    let age = u8::from_str(&age_as_string).unwrap_or(0);
-    let person = Person {
-        first_name,
-        last_name,
-        age,
-    };
-    Ok(person)
-}
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,24 @@
-use std::str::FromStr;
+use std::fs::{read_to_string, write};
+use std::str::{FromStr, Split};
+
+pub struct Person {
+    pub first_name: String,
+    pub last_name: String,
+    pub age: u8,
+}
+ impl Person {
+    
+    pub fn new() -> Self {
+        let first_name = ask_for_a_string("What is your first name?");
+        let last_name = ask_for_a_string("What is your last name?");
+        let age = ask_for_a_number("How old are you?").unwrap_or(0);
+        Person {
+            first_name,
+            last_name,
+            age,
+        }
+    }
+}
 
 pub fn read_string() -> String {
     let mut input = String::new();
@@ -35,4 +55,47 @@ pub fn ask_for_a_number(question: &str) -> Result<u8, String> {
 pub fn ask_for_a_string(question: &str) -> String {
     ask(question);
     read_string()
+}
+
+
+
+pub fn read_person(lines: &mut Split<char>) -> Option<Person> {
+    
+    let first_name = lines.next()?;
+    let last_name = lines.next()?;
+    let age_as_string = lines.next()?;
+    let age = u8::from_str(&age_as_string).unwrap_or(0);
+    let person = Person {
+        first_name: first_name.to_string(),
+        last_name: last_name.to_string(),
+        age,
+    };
+    Some(person)
+}
+
+    
+pub fn read_people() -> Result<Vec<Person>, std::io::Error> {
+    let input = read_to_string("people.txt")?;
+    let mut lines = input.split('\n');
+    let mut people = vec![];
+    while let Some(person) = read_person(&mut lines) {
+        people.push(person)
+    }
+    Ok(people)
+}
+
+//adding new function that writes user input to .txt file; use push_str to add user input into string
+//std::io:Result<()> is the output we are returning
+pub fn write_people(people: Vec<Person>) -> std::io::Result<()> {
+    let mut output = String::new();
+
+    for person in people {
+    output.push_str(&person.first_name);
+    output.push('\n');
+    output.push_str(&person.last_name);
+    output.push('\n');
+    output.push_str(&person.age.to_string());
+    output.push('\n');
+     }
+    write("people.txt", output)
 }
